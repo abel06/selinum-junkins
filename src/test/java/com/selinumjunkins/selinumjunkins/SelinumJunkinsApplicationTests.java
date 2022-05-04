e453c7b626b3d6993acdc2600f0d880c8e95caa4
abel <abelmesfin2003@gmail.com>
2022-05-04 14:42:57 +0200

    first commit

---
+++ src/test/java/com/selinumjunkins/selinumjunkins/SelinumJunkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selinumjunkins.selinumjunkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelinumJunkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
